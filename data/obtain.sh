134b6eb0ffdb77b1f674870a6d53e6d6e8edabf6
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2011-10-30 05:08:41 +0000

    Superficial changes to obtain.sh.

--- data/obtain.sh
+++ data/obtain.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo -n "Getting enwik8... "
+echo "Getting enwik8... "
 curl http://cs.fit.edu/~mmahoney/compression/enwik8.bz2 | bzip2 -d > enwik8
-echo "DONE
 echo "Getting enwik9... "
 curl http://cs.fit.edu/~mmahoney/compression/enwik9.bz2 | bzip2 -d > enwik9
-echo "DONE
