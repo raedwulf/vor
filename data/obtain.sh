ebd23e3cdade2786f0bcef389b0a7ef28d491735
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-11-23 11:46:42 +0000

    Remove old data script.

--- data/obtain.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Getting enwik8... "
-curl http://cs.fit.edu/~mmahoney/compression/enwik8.bz2 | bzip2 -d > enwik8
-echo "Getting enwik9... "
-curl http://cs.fit.edu/~mmahoney/compression/enwik9.bz2 | bzip2 -d > enwik9
